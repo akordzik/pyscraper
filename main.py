b775f13c669b8ba2ae94356022d49a10c0175d11
Artur Kordzik <akordzik@outlook.com>
2020-06-28 17:22:47 +0200

    updated link, key and added expired_at

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from bs4 import BeautifulSoup
 from decimal import Decimal
 from pymongo.database import Database
 from typing import List
+from datetime import datetime
 
 
 class Advertisement:
@@ -24,7 +25,7 @@ class Advertisement:
     def link(self) -> str:
         header = self.adv.find('header', class_='offer-item-header')
         link = header.find('a')
-        return link.attrs['href']
+        return str.split(link.attrs['href'], '#')[0]
 
     def rooms(self) -> int:
         li = self.adv_details.find('li', class_='offer-item-rooms')
@@ -45,35 +46,24 @@ class Advertisement:
     def key(self) -> int:
         return int(hashlib.sha256(self.link().encode('utf-8')).hexdigest(), 16) % 10**8
 
-    def to_dict(self) -> dict:
-        return {
-            'key': self.key(),
-            'area': self.area(),
-            'price_per_m': self.price_per_m(),
-            'price': self.price(),
-            'rooms': self.rooms(),
-            'link': self.link(),
-            'title': self.title(),
-        }
-
 
 def try_upsert(collection: Collection, advertisement: Advertisement) -> UpdateResult:
     return collection.update_one({'_id': advertisement.key()}, [
         {
-            "$replaceRoot": {
-                "newRoot": {
-                    "$mergeObjects": [
-                        "$$ROOT", {
-                            "price_historical": {
-                                "$ifNull": [
+            '$replaceRoot': {
+                'newRoot': {
+                    '$mergeObjects': [
+                        '$$ROOT', {
+                            'price_historical': {
+                                '$ifNull': [
                                     {
-                                        "$concatArrays": [
-                                            "$$ROOT.price_historical",
+                                        '$concatArrays': [
+                                            '$$ROOT.price_historical',
                                                 {
-                                                    "$cond": {"if": {"$eq": ["$$ROOT.price", advertisement.price()]}, "then": [], "else": [
+                                                    '$cond': {'if': {'$eq': ['$$ROOT.price', advertisement.price()]}, 'then': [], 'else': [
                                                         {
-                                                            "price": advertisement.price(),
-                                                            "updated_at": "$$NOW"
+                                                            'price': advertisement.price(),
+                                                            'updated_at': '$$NOW'
                                                         }
                                                     ]}
                                                 }
@@ -81,72 +71,101 @@ def try_upsert(collection: Collection, advertisement: Advertisement) -> UpdateRe
                                     },
                                     [
                                         {
-                                            "price": advertisement.price(),
-                                            "updated_at": "$$NOW"
+                                            'price': advertisement.price(),
+                                            'updated_at': '$$NOW'
                                         }
                                     ]
                                 ]
                             },
-                            "title": advertisement.title(),
-                            "link": advertisement.link(),
-                            "rooms": advertisement.rooms(),
-                            "area": advertisement.area(),
-                            "added_at": {
-                                "$ifNull": ["$$ROOT.added_at", "$$NOW"]
+                            'title': advertisement.title(),
+                            'link': advertisement.link(),
+                            'rooms': advertisement.rooms(),
+                            'area': advertisement.area(),
+                            'added_at': {
+                                '$ifNull': ['$$ROOT.added_at', '$$NOW']
                             }
                         }
                     ]
                 }
             }
         }, {
-            "$set": {
-                "price_per_m": advertisement.price_per_m(),
-                "price": advertisement.price()
+            '$set': {
+                'price_per_m': advertisement.price_per_m(),
+                'price': advertisement.price()
             }
         }
     ], upsert=True)
 
 
+def try_mark_outdated(scraped_ids: list, collection: Collection) -> List[dict]:
+    find = {'$and': [
+        {'_id': {'$not': {'$in': scraped_ids}}},
+        {'expired_at': {'$exists': False}}
+    ]}
+    to_be_updated = list(collection.find(find))
+
+    if len(to_be_updated) > 0:
+        collection.update_many(find, {
+            '$set': {'expired_at': datetime.now()}
+        })
+
+    return to_be_updated
+
+
 def main():
     client = pymongo.MongoClient(os.environ['MONGOLAB_URI'])
-    db: Database = client.get_default_database()
+    db = client.get_default_database()
     collection = db.get_collection('advertisements')
-    url = 'https://www.otodom.pl/sprzedaz/mieszkanie/?search%5Bfilter_float_price%3Ato%5D=800000&search%5Bfilter_float_price_per_m%3Ato%5D=12000&search%5Bfilter_float_m%3Afrom%5D=40&search%5Bfilter_float_m%3Ato%5D=70&search%5Bfilter_enum_floor_no%5D%5B0%5D=floor_1&search%5Bfilter_enum_floor_no%5D%5B1%5D=floor_2&search%5Bfilter_enum_floor_no%5D%5B2%5D=floor_3&search%5Bfilter_enum_floor_no%5D%5B3%5D=floor_4&search%5Bfilter_enum_floor_no%5D%5B4%5D=floor_5&search%5Bfilter_enum_floor_no%5D%5B5%5D=floor_6&search%5Bfilter_enum_floor_no%5D%5B6%5D=floor_7&search%5Bfilter_enum_floor_no%5D%5B7%5D=floor_8&search%5Bfilter_enum_floor_no%5D%5B8%5D=floor_9&search%5Bfilter_enum_floor_no%5D%5B9%5D=floor_10&search%5Bfilter_enum_floor_no%5D%5B10%5D=floor_higher_10&search%5Bfilter_enum_floor_no%5D%5B11%5D=garret&search%5Bdescription%5D=1&search%5Bprivate_business%5D=private&search%5Border%5D=created_at_first%3Adesc&locations%5B0%5D%5Bregion_id%5D=7&locations%5B0%5D%5Bsubregion_id%5D=197&locations%5B0%5D%5Bcity_id%5D=26&locations%5B0%5D%5Bdistrict_id%5D=42&locations%5B1%5D%5Bregion_id%5D=7&locations%5B1%5D%5Bsubregion_id%5D=197&locations%5B1%5D%5Bcity_id%5D=26&locations%5B1%5D%5Bdistrict_id%5D=847&locations%5B2%5D%5Bregion_id%5D=7&locations%5B2%5D%5Bsubregion_id%5D=197&locations%5B2%5D%5Bcity_id%5D=26&locations%5B2%5D%5Bdistrict_id%5D=39&locations%5B3%5D%5Bregion_id%5D=7&locations%5B3%5D%5Bsubregion_id%5D=197&locations%5B3%5D%5Bcity_id%5D=26&locations%5B3%5D%5Bdistrict_id%5D=44&locations%5B4%5D%5Bregion_id%5D=7&locations%5B4%5D%5Bsubregion_id%5D=197&locations%5B4%5D%5Bcity_id%5D=26&locations%5B4%5D%5Bdistrict_id%5D=724&locations%5B5%5D%5Bregion_id%5D=7&locations%5B5%5D%5Bsubregion_id%5D=197&locations%5B5%5D%5Bcity_id%5D=26&locations%5B5%5D%5Bdistrict_id%5D=40&locations%5B6%5D%5Bregion_id%5D=7&locations%5B6%5D%5Bsubregion_id%5D=197&locations%5B6%5D%5Bcity_id%5D=26&locations%5B6%5D%5Bdistrict_id%5D=53&locations%5B7%5D%5Bregion_id%5D=7&locations%5B7%5D%5Bcity_id%5D=26&locations%5B7%5D%5Bdistrict_id%5D=3319&locations%5B8%5D%5Bregion_id%5D=7&locations%5B8%5D%5Bcity_id%5D=26&locations%5B8%5D%5Bdistrict_id%5D=38&nrAdsPerPage=72'
+    urls = [
+        'https://www.otodom.pl/sprzedaz/mieszkanie/?search%5Bfilter_float_price%3Ato%5D=800000&search%5Bfilter_float_price_per_m%3Ato%5D=12000&search%5Bfilter_float_m%3Afrom%5D=40&search%5Bfilter_float_m%3Ato%5D=70&search%5Bfilter_enum_floor_no%5D%5B0%5D=floor_1&search%5Bfilter_enum_floor_no%5D%5B1%5D=floor_2&search%5Bfilter_enum_floor_no%5D%5B2%5D=floor_3&search%5Bfilter_enum_floor_no%5D%5B3%5D=floor_4&search%5Bfilter_enum_floor_no%5D%5B4%5D=floor_5&search%5Bfilter_enum_floor_no%5D%5B5%5D=floor_6&search%5Bfilter_enum_floor_no%5D%5B6%5D=floor_7&search%5Bfilter_enum_floor_no%5D%5B7%5D=floor_8&search%5Bfilter_enum_floor_no%5D%5B8%5D=floor_9&search%5Bfilter_enum_floor_no%5D%5B9%5D=floor_10&search%5Bfilter_enum_floor_no%5D%5B10%5D=floor_higher_10&search%5Bfilter_enum_floor_no%5D%5B11%5D=garret&search%5Bdescription%5D=1&search%5Bprivate_business%5D=private&search%5Border%5D=created_at_first%3Adesc&locations%5B0%5D%5Bregion_id%5D=7&locations%5B0%5D%5Bsubregion_id%5D=197&locations%5B0%5D%5Bcity_id%5D=26&locations%5B0%5D%5Bdistrict_id%5D=42&locations%5B1%5D%5Bregion_id%5D=7&locations%5B1%5D%5Bsubregion_id%5D=197&locations%5B1%5D%5Bcity_id%5D=26&locations%5B1%5D%5Bdistrict_id%5D=847&locations%5B2%5D%5Bregion_id%5D=7&locations%5B2%5D%5Bsubregion_id%5D=197&locations%5B2%5D%5Bcity_id%5D=26&locations%5B2%5D%5Bdistrict_id%5D=39&locations%5B3%5D%5Bregion_id%5D=7&locations%5B3%5D%5Bsubregion_id%5D=197&locations%5B3%5D%5Bcity_id%5D=26&locations%5B3%5D%5Bdistrict_id%5D=44&locations%5B4%5D%5Bregion_id%5D=7&locations%5B4%5D%5Bsubregion_id%5D=197&locations%5B4%5D%5Bcity_id%5D=26&locations%5B4%5D%5Bdistrict_id%5D=724&locations%5B5%5D%5Bregion_id%5D=7&locations%5B5%5D%5Bsubregion_id%5D=197&locations%5B5%5D%5Bcity_id%5D=26&locations%5B5%5D%5Bdistrict_id%5D=40&locations%5B6%5D%5Bregion_id%5D=7&locations%5B6%5D%5Bsubregion_id%5D=197&locations%5B6%5D%5Bcity_id%5D=26&locations%5B6%5D%5Bdistrict_id%5D=53&locations%5B7%5D%5Bregion_id%5D=7&locations%5B7%5D%5Bcity_id%5D=26&locations%5B7%5D%5Bdistrict_id%5D=3319&locations%5B8%5D%5Bregion_id%5D=7&locations%5B8%5D%5Bcity_id%5D=26&locations%5B8%5D%5Bdistrict_id%5D=38&nrAdsPerPage=72',
+        'https://www.otodom.pl/sprzedaz/mieszkanie/?search%5Bfilter_float_price%3Ato%5D=700000&search%5Bfilter_float_price_per_m%3Ato%5D=10000&search%5Bfilter_float_m%3Afrom%5D=40&search%5Bfilter_float_m%3Ato%5D=70&search%5Bfilter_enum_floor_no%5D%5B0%5D=floor_1&search%5Bfilter_enum_floor_no%5D%5B1%5D=floor_2&search%5Bfilter_enum_floor_no%5D%5B2%5D=floor_3&search%5Bfilter_enum_floor_no%5D%5B3%5D=floor_4&search%5Bfilter_enum_floor_no%5D%5B4%5D=floor_5&search%5Bfilter_enum_floor_no%5D%5B5%5D=floor_6&search%5Bfilter_enum_floor_no%5D%5B6%5D=floor_7&search%5Bfilter_enum_floor_no%5D%5B7%5D=floor_8&search%5Bfilter_enum_floor_no%5D%5B8%5D=floor_9&search%5Bfilter_enum_floor_no%5D%5B9%5D=floor_10&search%5Bfilter_enum_floor_no%5D%5B10%5D=floor_higher_10&search%5Bfilter_enum_floor_no%5D%5B11%5D=garret&search%5Bdescription%5D=1&search%5Bprivate_business%5D=private&search%5Border%5D=created_at_first%3Adesc&locations%5B0%5D%5Bregion_id%5D=7&locations%5B0%5D%5Bsubregion_id%5D=197&locations%5B0%5D%5Bcity_id%5D=26&locations%5B0%5D%5Bdistrict_id%5D=41&locations%5B1%5D%5Bregion_id%5D=7&locations%5B1%5D%5Bsubregion_id%5D=197&locations%5B1%5D%5Bcity_id%5D=26&locations%5B1%5D%5Bdistrict_id%5D=42&locations%5B2%5D%5Bregion_id%5D=7&locations%5B2%5D%5Bsubregion_id%5D=197&locations%5B2%5D%5Bcity_id%5D=26&locations%5B2%5D%5Bdistrict_id%5D=847&locations%5B3%5D%5Bregion_id%5D=7&locations%5B3%5D%5Bsubregion_id%5D=197&locations%5B3%5D%5Bcity_id%5D=26&locations%5B3%5D%5Bdistrict_id%5D=39&locations%5B4%5D%5Bregion_id%5D=7&locations%5B4%5D%5Bsubregion_id%5D=197&locations%5B4%5D%5Bcity_id%5D=26&locations%5B4%5D%5Bdistrict_id%5D=47&locations%5B5%5D%5Bregion_id%5D=7&locations%5B5%5D%5Bsubregion_id%5D=197&locations%5B5%5D%5Bcity_id%5D=26&locations%5B5%5D%5Bdistrict_id%5D=117&nrAdsPerPage=72'
+    ]
 
     print('Started scraping')
+
     try:
         upserted_adverts = []
         modified_adverts: List[Advertisement] = []
-        page = 1
+        scraped_ids = []
 
-        while True:
-            url_page = f'&page={page}' if page > 1 else ''
-            page_content = requests.get(url + url_page, allow_redirects=False)
+        for url in urls:
+            page = 1
 
-            if page_content.status_code > 299:
-                break
+            while True:
+                url_page = f'&page={page}' if page > 1 else ''
+                page_content = requests.get(
+                    url + url_page, allow_redirects=False)
 
-            soup = BeautifulSoup(page_content.content, 'html.parser')
-            container = soup.find(id='body-container')
-            articles = container.find_all('article')
+                if page_content.status_code > 299:
+                    break
 
-            for article in articles[3:]:
-                advertisement = Advertisement(article)
-                result = try_upsert(collection, advertisement)
+                soup = BeautifulSoup(page_content.content, 'html.parser')
+                container = soup.find(id='body-container')
+                articles = container.find_all('article')
+                adverts = list(map(lambda x: Advertisement(x), articles[3:]))
 
-                if result.modified_count > 0:
-                    modified_adverts.append(advertisement)
+                for advertisement in adverts:
+                    result = try_upsert(collection, advertisement)
 
-                if result.upserted_id != None:
-                    upserted_adverts.append(advertisement)
+                    if result.modified_count > 0:
+                        modified_adverts.append(advertisement)
+                    elif result.upserted_id != None:
+                        upserted_adverts.append(advertisement)
 
-            page = page + 1
+                    scraped_ids.append(advertisement.key())
+
+                page = page + 1
+
+        outdated_adverts = try_mark_outdated(scraped_ids, collection)
 
         for a in modified_adverts:
             print(f'Modified: {a.title()}')
 
         for a in upserted_adverts:
             print(f'Upserted: {a.title()}')
+
+        for a in outdated_adverts:
+            print(f'Outdated: {a["title"]}')
     finally:
         print('Finished scraping')
         client.close()
